9375c89ec03f76c9a37d9e43bee0b1956e6052f4
xinwen_liang@brown.edu <xliang34@node1747.oscar.ccv.brown.edu>
2024-07-17 00:20:19 -0400

    inothing to updata Please enter the commit message for your changes. Lines starting

--- main_code/multicore.R
+++ main_code/multicore.R
@@ -3,11 +3,15 @@ library(doParallel)
 library(iterators)
 library(peakRAM)
 library(devtools)
+packages <- c('MSFA', 'peakRAM', 'BFR.BE', 'tidyverse', 'matlab', 'MatrixCorrelation',"Rcpp","SUFA")
+lapply(packages, library, character.only = TRUE)
 source("./functions/gen_senerioSS.R")
 source("./functions/gen_senerioBMSFA.R")
-source("./main_code/sim_once.R")
+source("./functions/calculateRV.R")
+source("./functions/post_BMSFA.R")
+source("./functions/post_PFA.R")
 source("./functions/measurements.R")
-packages <- c('MSFA', 'peakRAM', 'BFR.BE', 'tidyverse', 'matlab', 'MatrixCorrelation') ## MSFA used is the Mavis version
+source("./functions/post_SUFA.R")
 
 
 # Detect the number of cores
@@ -18,39 +22,45 @@ cl <- makeCluster(numCores - 3)
 registerDoParallel(cl)
 
 ###############################Senerio 1 Dense Phi##############################
-#sen1_dense <- foreach(i = 1:50, .combine = 'rbind',
-       #            .packages = packages) %dopar% {
-  
-      #                             data_sen1 <- gen_senerioSS(S=4, N=500, P=50, Q=2, K=5)
-     #                              results <- fitting(data_sen1)
+sen1_dense <- foreach(i = 1:5050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050, .combine = 'rbind',
+                      # "QiupC" is cpp function sourced in PFA.R, and should be import separately.
+                   .packages = packages, .noexport = c("QiupC")) %dopar% {
+                                   data_sen1 <- gen_senerioSS(S=4, N=500, P=50, Q=2, K=5)
+                                   results <- fitting(data_sen1)
 
                                    # RV and FN for Phi
-    #                                metrices <- est_perform(data_sen1, results$result_BMSFA, results$result_MOMSS)
+                                    metrices <- est_perform(data_sen1, results)
                                    # Computing performance
-   #                                 comp_metrices <- computing_perform(results[c("profile_MOMSS", "profile_BMSFA")])
+                                    comp_metrices <- computing_perform(results[c("profile_MOMSS", "profile_BMSFA", 
+                                                                                 "profile_PFA", "profile_SUFA")])
 
                                    # Output
-  #                                 c(metrices, comp_metrices)
- #                                }
-#saveRDS(sen1_dense, "./sen1_dense.rds")
+                                   c(metrices, comp_metrices)
+                                 }
+saveRDS(sen1_dense, "./sen1_dense.rds")
+# Stop the parallel backend after the job is done
+stopImplicitCluster()
 ###############################################################################
 
 ##########################Senerio 1 Sparse Phi#################################
-sen1_sparse <- foreach(i = 1:50, .combine = 'rbind', 
-                      .packages = packages) %dopar% {
-                                      data_sen1 <- gen_senerioSS(S=4, N=500, P=50, Q=2, K=5, genPhi = "sparse")
-                                      results <- fitting(data_sen1)
-                                      
-                                      # RV and FN for Phi
-                                      metrices <- est_perform(data_sen1, results$result_BMSFA, results$result_MOMSS)
-                                      # Computing performance
-                                      comp_metrices <- computing_perform(results[c("profile_MOMSS", "profile_BMSFA")])
-                                      
-                                      # Output
-                                      c(metrices, comp_metrices)
-                                    }
-saveRDS(sen1_sparse, "./sen1_sparse.rds")
+# registerDoParallel(cl)
+# sen1_sparse <- foreach(i = 1:50, .combine = 'rbind', 
+#                       .packages = packages) %dopar% {
+#                                       data_sen1 <- gen_senerioSS(S=4, N=500, P=50, Q=2, K=5, genPhi = "sparse")
+#                                       results <- fitting(data_sen1)
+#                                       
+#                                       # RV and FN for Phi
+#                                       metrices <- est_perform(data_sen1, results$result_BMSFA, results$result_MOMSS, results$result_PFA)
+#                                       # Computing performance
+#                                       comp_metrices <- computing_perform(results[c("profile_MOMSS", "profile_BMSFA", "profile_PFA")])
+#                                       
+#                                       # Output
+#                                       c(metrices, comp_metrices)
+#                                     }
+# saveRDS(sen1_sparse, "./sen1_sparse.rds")
+# # Stop the parallel backend after the job is done
+# stopImplicitCluster()
 ###############################################################################
 
-# Stop the parallel backend after the job is done
-stopImplicitCluster()
+
+
