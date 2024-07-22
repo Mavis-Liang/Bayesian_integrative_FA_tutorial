f0757f5932a2dd35bd9f35b7bb536538f4a90a56
xinwen_liang@brown.edu <xliang34@node1735.oscar.ccv.brown.edu>
2024-07-22 05:26:33 -0400

    testing SUFA

--- run_R.sh
+++ run_R.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #SBATCH -n 16
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH -t 30:00:00
 
-module load r/4.2.2-z6qdiis
-Rscript ./main_code/multicore.R
+run_R.sh
+
+module load r/4.4.0-yycctsj
+Rscript ./main_code/sim_once.R 
